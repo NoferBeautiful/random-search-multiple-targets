f0c3f631846f284cb2f20b82da6ff1003290755d
TrandeLik <feoctistov1580@yandex.ru>
2022-11-17 21:53:48 +0300

    main pages

--- source/main.py
+++ source/main.py
@@ -1,14 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QGraphicsScene, QSlider
-from PyQt6 import uic
-from PyQt6.QtCore import QTimer
-import sys
-import numpy as np
-
-from grid import Grid
-from point import Point
 from ui import UI
 import env
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     UI = UI()
     UI.start_exe()
